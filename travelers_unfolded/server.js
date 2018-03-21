aae702825b0751765c370461ead34acfa27b7d2a
tnangel1984 <tnangel1984123@gmail.com>
2018-03-21 16:50:01 -0400

    corrected middleware connections. Created test index route with res send.  Test works

--- travelers_unfolded/server.js
+++ travelers_unfolded/server.js
@@ -4,9 +4,7 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, ()=>{
-  console.log("Express Listening...");
-})
+
 
 const mongoose = require ('mongoose');
 const mongoURI = ('mongodb://localhost/TravelersDB');
@@ -14,28 +12,38 @@ mongoose.connect(mongoURI);
 
 mongoose.connection.once('open',()=>{
   console.log("Connected to Mongo");
-} )
+});
 
 
 
 //MIDDLE WARE
-app.use(express.urlencoded{extended:false}); //<--bodyparser for body object
+app.use(express.urlencoded({extended:false})); //<--bodyparser for body object
 app.use(express.static('public')); //<--for CSS
 
-const methodOverride = express('method-override');
-  app.use(methodOverride('_mmethod'));  //<-- for put/delete methods
+const methodOverride = require('method-override');
+  app.use(methodOverride('_method'));  //<-- for put/delete methods
 
 const session = require('express-session'); //<-- for sessions object
-  app.use(
+  app.use(session(
     {secret: "feedmeseymour",
      resave: false,
      saveunitialized:false
 
     }
-  );
+  ));
 
 
 
 
 
 //ROUTES
+
+app.get('/', (req, res)=>{
+   res.send("index page goes here");
+});
+
+
+
+app.listen(3000, ()=>{
+  console.log('Express Listening...');
+});
