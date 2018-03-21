1ca2e7845351e564e4e0ac196446aaf4620e9fee
tnangel1984 <tnangel1984123@gmail.com>
2018-03-21 17:10:17 -0400

    connected destinations router. Created dest index route and tested render

--- travelers_unfolded/controllers/destinations.js
+++ travelers_unfolded/controllers/destinations.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 
-
-
+//Destination Page ROUTERS
+//index
+router.get('/', (req, res)=>{
+  res.render('destinations/index.ejs')
+});
 
 module.exports = router;
--- travelers_unfolded/server.js
+++ travelers_unfolded/server.js
@@ -1,36 +1,44 @@
 //Server
 
-//Package Connections
-const express = require('express');
-const app = express();
+    //Main Package Connections
+    const express = require('express');
+    const app = express();
+
+    app.listen(3000, ()=>{
+      console.log('Express Listening...');
+    });
+
+    const mongoose = require ('mongoose');
+    const mongoURI = ('mongodb://localhost/TravelersDB');
+    mongoose.connect(mongoURI);
+
+    mongoose.connection.once('open',()=>{
+      console.log("Connected to Mongo");
+    });
 
 
+//CONTROLLERS
+    const destinationController = require("./controllers/destinations.js");
+    app.use('/destinations/', destinationController);
 
-const mongoose = require ('mongoose');
-const mongoURI = ('mongodb://localhost/TravelersDB');
-mongoose.connect(mongoURI);
-
-mongoose.connection.once('open',()=>{
-  console.log("Connected to Mongo");
-});
 
 
 
 //MIDDLE WARE
-app.use(express.urlencoded({extended:false})); //<--bodyparser for body object
-app.use(express.static('public')); //<--for CSS
+    app.use(express.urlencoded({extended:false})); //<--bodyparser for body object
+    app.use(express.static('public')); //<--for CSS
 
-const methodOverride = require('method-override');
-  app.use(methodOverride('_method'));  //<-- for put/delete methods
+    const methodOverride = require('method-override');
+    app.use(methodOverride('_method'));  //<-- for put/delete methods
 
-const session = require('express-session'); //<-- for sessions object
-  app.use(session(
+    const session = require('express-session'); //<-- for sessions object
+    app.use(session(
     {secret: "feedmeseymour",
      resave: false,
      saveunitialized:false
 
     }
-  ));
+    ));
 
 
 
@@ -40,10 +48,4 @@ const session = require('express-session'); //<-- for sessions object
 
 app.get('/', (req, res)=>{
    res.render('index.ejs');
-});
-
-
-
-app.listen(3000, ()=>{
-  console.log('Express Listening...');
 });
