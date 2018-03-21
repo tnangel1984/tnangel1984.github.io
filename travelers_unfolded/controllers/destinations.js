5cdca0906cf132343a2f9ec3c5227755d136837a
tnangel1984 <tnangel1984123@gmail.com>
2018-03-21 17:44:48 -0400

    connected model and corrected destSchema

--- travelers_unfolded/controllers/destinations.js
+++ travelers_unfolded/controllers/destinations.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Destination = require('../models/destinations.js');
 
 
 //Destination Page ROUTERS
--- travelers_unfolded/models/destinations.js
+++ travelers_unfolded/models/destinations.js
@@ -4,15 +4,15 @@ const Schema = mongoose.Schema;
 
 const destinationSchema = new Schema(
   {
-   city:{String, required:true},
-   country:{String,required:false},
+   city: {type:String, required:true},
+   country: {type:String,required:false},
    stateRegion:String,
    dateOfVisit:String,
    img1:[String], //<--- test using string of arrays to populate slide show..
    img2:String,
-   img3:String
-   recommendations:
-   description:
+   img3:String,
+   recommendations:String,
+   description:String,
    tags:[String]
 
   }
