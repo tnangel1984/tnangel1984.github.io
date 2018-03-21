b541305ef31fd896c564b1d2e4783df296f0afba
tnangel1984 <tnangel1984123@gmail.com>
2018-03-21 16:15:42 -0400

    established server connections, and middleware connections in server.js

--- travelers_unfolded/server.js
+++ travelers_unfolded/server.js
@@ -3,6 +3,7 @@
 //Package Connections
 const express = require('express');
 const app = express();
+
 app.listen(3000, ()=>{
   console.log("Express Listening...");
 })
@@ -16,4 +17,25 @@ mongoose.connection.once('open',()=>{
 } )
 
 
+
 //MIDDLE WARE
+app.use(express.urlencoded{extended:false}); //<--bodyparser for body object
+app.use(express.static('public')); //<--for CSS
+
+const methodOverride = express('method-override');
+  app.use(methodOverride('_mmethod'));  //<-- for put/delete methods
+
+const session = require('express-session'); //<-- for sessions object
+  app.use(
+    {secret: "feedmeseymour",
+     resave: false,
+     saveunitialized:false
+
+    }
+  );
+
+
+
+
+
+//ROUTES
