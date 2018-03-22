a766114ea51fee588e11fb25c24a8a902f128634
tnangel1984 <tnangel1984123@gmail.com>
2018-03-22 17:53:45 -0400

    seeded 2 more posts

--- models/seedDestinations.js
+++ models/seedDestinations.js
@@ -15,6 +15,37 @@ let seedDestinations = [
 
         description:"My very first trip to China would've have been complete without a trip to The Great Wall (one of the seven wonders of the world). I stayed out till 4 a.m., and had to be up at 7 for the tour, but it was so worth it. Unfortunately the night before while walking through the night market (also pictured) a guy swept trash on me and i almost slipped, but successfully managed to sprain my knee just hours before I would be hiking up The Wall. But I was determined and the journey to the top was well worth the view. The pictures don't do it justice. I can't even begin to describe how beautiful and peaceful it is standing on the wall surrounded by nature creation of mountains abound. Frankly, I think nature was competing with the wall... and won.",
         tags:['Great Wall', 'Mountains', 'WanFuXing', 'Beijing']
+      },
+      {
+        city: 'Positano',
+        country: 'Italy',
+        stateRegion:"Almafi Coast",
+        dateOfVisit:'2016',
+
+        img1:"https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/13256180_10101947669252458_516914382951365887_n.jpg?_nc_cat=0&oh=aeff9b5d10cf913560506b9917bbab38&oe=5B3ED0F7",
+        img2:"https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/13237783_10101946958995818_2372151067029816243_n.jpg?_nc_cat=0&oh=95d113b6c0c430188e86fca642ee72e2&oe=5B3135E2",
+        img3:"#",
+
+        recco:"A must stop! Stay at Alciano great view and very affordable, don't wase your money at the Posideon (it's overpriced plus you have to walk up millions of stairs to get there). Skip Almafi, but if you're still curious its only 20 minutes away",
+
+        description:"Fell in love with this little town on what was supposed to be a 3-country tour: France, Italy, and Spain. Unfortunately, I never made it to Spain. The town was so beautiful and the weather was great. I spent the remainder of my vacation having lazy breakfasts on the balcony , browsing the shops, walking down to the beach everyday and occasionally hoping the bus to see other towns like Ravello ...and yes Almafi",
+        tags:['Positano', 'Almafi Coast', 'Beach']
+      },
+      {
+        city: 'Niagra Falls',
+        country: 'Canada',
+        stateRegion:"Ontario",
+        dateOfVisit:'2016',
+
+        img1:"https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/14068149_10102022921935478_6499186200129254347_n.jpg?oh=f5123828910259a091b1fd27c955eed8&oe=5B48BAAD",
+        img2:"https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/14192015_10102022921780788_6496178016394129566_n.jpg?_nc_cat=0&oh=511674724e51c3ec9d0288f7d5d7c217&oe=5B415231",
+        img3:"https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/14142023_10102021974179788_7782693942438124746_n.jpg?oh=cb9ec0fb9a97ce7ad3dbd525dd40eeed&oe=5B2BFFBC",
+
+        recco:"Take tour bus from Toronto! It's $60 bucks, extra for add-ons but worth it!",
+
+        description:"My 3Xth birthday! I rarely celebrate my birthday so when I do I like to do something amazing and memorable. Well my trip to Niagra falls definitely surpassed expectations. We stayed in Toronto booked a $60 tour buss that taked you to the falls, wineries, and Niagra on the Lake - the helicopter ride was ofcourse extra but it was worth every dime and was the highlight of the experience. It was me and one of my best friends having the most amazing 30 minutes ever (haha) Unfortunately, I was too excited to take any good pictures, but the memory of the views will last a life time. ",
+
+        tags:['Positano', 'Almafi Coast', 'Beach']
       }
     ]
       // {
