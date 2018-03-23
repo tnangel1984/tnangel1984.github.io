f21d1330f25c9bad4e8edae455d4addde8c317be
tnangel1984 <tnangel1984123@gmail.com>
2018-03-23 16:13:19 -0400

    Expanding Homepage HTML

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -12,13 +12,9 @@ router.get('/new', (req, res)=>{
 router.post("/new", (req, res)=>{
     Traveler.findOne({username:req.body.username}, (err, foundUser)=>{
             //must do a bcrypt comparaon here because of encrption
-            console.log(foundUser);
-            console.log(req.session);
-            console.log(bcrypt.compareSync(req.body.password,foundUser.password));
           if(bcrypt.compareSync(req.body.password, foundUser.password)){
               // create a session specific user
-            req.session.currentUser = foundUser;
-            console.log(req.session.currentUser);
+            req.session.currentUser = foundUser;  
             res.redirect('/destinations');
           }else{
             res.send("Error wrong password");
