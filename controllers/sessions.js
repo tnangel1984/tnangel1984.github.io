7d92c04fae60d5e1945f68a3d10a2c40aa6e61d2
tnangel1984 <tnangel1984123@gmail.com>
2018-03-22 20:56:44 -0400

    Added logout button and delete route, tested, Working

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
-
+const Traveler = require('../models/travelers.js');
+const bcrypt = require('bcrypt');
 //Sessions Page ROUTERS
 //new
 
@@ -9,7 +9,25 @@ router.get('/new', (req, res)=>{
   res.render('sessions/new.ejs');
 })
 
+router.post("/new", (req, res)=>{
+    Traveler.findOne({username:req.body.username}, (err, foundUser)=>{
+            //must do a bcrypt comparaon here because of encrption
+          if(bcrypt.compareSync(req.body.password, foundUser.password)){
+              // create a session specific user
+            req.session.currentuser = foundUser;
+            res.redirect('/destinations');
+          }else{
+            res.send("Error wrong password");
+          }
+    });
+});
 
+
+router.delete('/', (req, res)=>{
+   req.session.destroy((err)=>{
+     res.redirect('/');
+   })
+})
 
 
 
