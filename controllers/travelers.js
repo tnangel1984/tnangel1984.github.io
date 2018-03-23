a4f5dd3a743b332bf82754c2dc0df5f8fdb29fad
tnangel1984 <tnangel1984123@gmail.com>
2018-03-22 20:07:52 -0400

    Added encrption to new password registration

--- controllers/travelers.js
+++ controllers/travelers.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Traveler = require('../models/travelers.js');
+const bcrypt = require('bcrypt');
+
 
 //Travelers Page ROUTERS
 //index
@@ -15,6 +17,7 @@ router.get('/new', (req, res)=>{
 
 //post/Create
 router.post('/new', (req, res)=>{
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
    Traveler.create(req.body, (err, createdTraveler)=>{
      res.redirect('/destinations');
    })
